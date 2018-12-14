50c34d095b172c2e9a827ad1bf4fa5b313cd82cc
ookimaki <ookimaki@givinglog.com>
2018-12-14 21:42:34 +0900

    add

--- plugins/init.js
+++ plugins/init.js
@@ -1,4 +1,6 @@
 import eosjs_ecc from 'eosjs-ecc'
+import EosManager from '~/assets/js/eos'
+const eosManager = new EosManager('https://api.kylin.alohaeos.com')
 
 if (process.browser) {
     if (localStorage.getItem('eosclip_account') == null || localStorage.getItem('eosclip_priveKey') == null) {
@@ -11,4 +13,31 @@ if (process.browser) {
     } else {
         console.log("Your Account: " + localStorage.getItem('eosclip_account'))
     }
+
+    window.onNuxtReady(async ({$store}) => {
+        
+        var questionParam = {
+            scope: "eosqarecove5",
+            code: "eosqarecove5",
+            table: 'question',
+            json: true,
+            limit: 100
+        }
+
+        var questions = await eosManager.read(questionParam);        
+        $store.commit("setQuestions", questions)
+
+        var answerParam = {
+            scope: "eosqarecove5",
+            code: "eosqarecove5",
+            table: 'question',
+            json: true,
+            limit: 100
+        }
+
+        var answers = await eosManager.read(answerParam);        
+        $store.commit("setAnswers", answers)
+
+    })
+
 }
--- store/index.js
+++ store/index.js
@@ -3,63 +3,19 @@ import Vuex from 'vuex'
 const createStore = () => {
     return new Vuex.Store({
       state: () => ({
-        userAccount: "daidaides",
-        balance: "2.0000 EOS",
-        pub_key: "pub",
-        prive_key: "5JieSd3S7SLadXS6uPdxgH2i87H4oTTxiKUHFa7eaqZHmd47gUD",
-        questions: [
-            {
-            question_key: "1",
-            title: "Hello",
-            body: "aaaaaaaaaaaa",
-            user_key: "0",
-            pub_key: "EOS7",
-            price: "0",
-            view: "200",
-            time_stamp: "2018-12-12",
-    
-          },
-          {
-            question_key: "2",
-            title: "world",
-            body: "bbbbbbbbbbbb",
-            user_key: "0",
-            pub_key: "EOS7",
-            price: "0",
-            view: "200",
-            time_stamp: "2018-12-12",
-          },
-            ],
-        answers: [{
-            answer_key: "1",
-            question_key: "1",
-            answer: "dasda",
-            user_key: "1",
-            pub_key: "EOS8",
-            point: "1.000",
-            time_stamp: "2018-12-13"
-            },
-            {
-            answer_key: "2",
-            question_key: "2",
-            answer: "uuuuuuu",
-            user_key: "1",
-            pub_key: "EOS8",
-            point: "1.000",
-            time_stamp: "2018-12-13"
-            },
-
-        ]
+        questions: [],
+        answers: []
       }),
+      
       mutations: {
-        setUserAccount (state, userAccount) {
-            state.userAccount = userAccount
+
+        setQuestions (state, questions) {
+            state.question = questions
         },
 
-        setPub_key (state, pub_key) {
-            state.pub_key = pub_key
-            console.log(state.pub_key)
-        }
+        setAnswers (state, answers) {
+          state.answers = answers
+        },
               
       }
     })
